6dd1acd9fcdd9a69ec44aec39dada0ef288ad400
Matt <matthew.thompson.a@gmail.com>
2020-01-18 01:58:18 +0000

    mer..height

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
     <Flex py={{ base: 5 }} justifyContent={{ base: 'center' }} bg="gray.900">
       <Image
         w={{ base: '60%', md: '275px' }}
+        h="unset"
         src={JsLouLogo}
         alt="JSLou Logo"
       />
