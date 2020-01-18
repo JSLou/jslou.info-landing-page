c20692e8e7f957bd0b840682a308f7ee32847e0b
Matt <matthew.thompson.a@gmail.com>
2020-01-18 02:08:56 +0000

    ngrok live test this dumb header...

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,9 @@ import JsLouLogo from '../public/images/logo-2015.png';
 const Header = () => {
   return (
     <Flex py={{ base: 5 }} justifyContent={{ base: 'center' }} bg="gray.900">
-      <Image
-        w={{ base: '60%', md: '275px' }}
-        h="unset"
-        src={JsLouLogo}
-        alt="JSLou Logo"
-      />
+      <Flex w="50%" maxH="180px" justifyContent="center">
+        <Image h="100%" src={JsLouLogo} alt="JSLou Logo" />
+      </Flex>
     </Flex>
   );
 };
